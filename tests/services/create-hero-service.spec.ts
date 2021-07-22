b6cb40412a77b0b33be7b7fc20fa4a1617dd826b
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-07-22 13:16:22 -0300

    test: ensure CreateHeroServiceService throws if createHeroRepository throws

--- tests/services/create-hero-service.spec.ts
+++ tests/services/create-hero-service.spec.ts
@@ -2,6 +2,7 @@ import { ICreateHeroRepository } from '@/contracts/repositories'
 import { ICreateHero } from '@/contracts/services'
 import { CreateHeroService } from '@/services/create-hero-service'
 import { mock, MockProxy } from 'jest-mock-extended'
+import { throwError } from '../helpers'
 
 describe('CreateHeroService', () => {
   let createHeroRepository: MockProxy<ICreateHeroRepository>
@@ -22,5 +23,11 @@ describe('CreateHeroService', () => {
     await sut.execute(hero)
     expect(createHeroRepository.create).toBeCalledWith(hero)
     expect(createHeroRepository.create).toHaveBeenCalledTimes(1)
+  })
+
+  it('Should throw if createHeroRepository throws', async () => {
+    createHeroRepository.create.mockRejectedValueOnce(throwError)
+    const promise = sut.execute(hero)
+    await expect(promise).rejects.toThrow()
   })
 })
