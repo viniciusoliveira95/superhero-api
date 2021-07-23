b356f4f4c8c3a5484d4265ff2e453b9ea62b15c3
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-07-23 19:14:54 -0300

    feat: add express and other configs files

---
+++ src/main/server.ts
@@ -0,0 +1,10 @@
+import 'module-alias/register'
+import env from './config/env'
+import { MongoHelper } from '@/repositories'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(env.port, () => console.log(`Server running at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
