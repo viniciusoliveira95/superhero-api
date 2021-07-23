2ef15a853605128ba12eda4fe7143371bda35bc8
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-07-23 19:41:43 -0300

    feat: add CreateHeroController factory

---
+++ src/main/adapters/hero/create-hero-controller-factory.ts
@@ -0,0 +1,12 @@
+import { IController } from '@/contracts/controllers'
+import { CreateHeroController } from '@/controllers/hero'
+import { HeroRepository } from '@/repositories'
+import { CreateHeroService } from '@/services'
+import { HeroRequestValidation } from '@/validations'
+
+export const makeCreateHeroController = (): IController => {
+  const heroRepository = new HeroRepository()
+  const createHeroService = new CreateHeroService(heroRepository, heroRepository, heroRepository)
+  const heroRequestValidation = new HeroRequestValidation()
+  return new CreateHeroController(createHeroService, heroRequestValidation)
+}
