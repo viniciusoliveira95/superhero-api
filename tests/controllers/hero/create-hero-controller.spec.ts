55af04c8ac849b08fb375dd50061c8f36ff7c7a9
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-07-23 14:25:20 -0300

    test: ensure CreateHeroController returns 403 errors when a property is already used

--- tests/controllers/hero/create-hero-controller.spec.ts
+++ tests/controllers/hero/create-hero-controller.spec.ts
@@ -1,5 +1,8 @@
 import { CreateHeroController } from '@/controllers/hero'
 import { ICreateHero } from '@/contracts/services'
+import { ServerError, PropertyInUseError } from '@/errors'
+import { forbidden, serverError } from '@/controllers/http-helper'
+
 import { mock, MockProxy } from 'jest-mock-extended'
 
 describe('CreateHero Controller', () => {
@@ -11,14 +14,43 @@ describe('CreateHero Controller', () => {
     rank: 1,
     active: true
   }
+  const serviceResponse: ICreateHero.Result = {
+    created: false,
+    nameAlreadyUsed: false,
+    rankAlreadyUsed: false
+  }
 
   beforeEach(() => {
     createHeroService = mock()
     sut = new CreateHeroController(createHeroService)
   })
 
-  it('Should call service with correct values', async () => {
+  it('Should call service execute with correct values', async () => {
     await sut.handle(params)
     expect(createHeroService.execute).toBeCalledWith(params)
+  })
+
+  it('Should return 500 if service execute throws', async () => {
+    createHeroService.execute.mockRejectedValueOnce(new Error())
+    const httpResponse = await sut.handle(params)
+    expect(httpResponse).toEqual(serverError(new ServerError(null)))
+  })
+
+  it('Should return 403 if nameAlreadyUsed is true', async () => {
+    createHeroService.execute.mockResolvedValueOnce({ ...serviceResponse, nameAlreadyUsed: true })
+    const httpResponse = await sut.handle(params)
+    expect(httpResponse).toEqual(forbidden(new PropertyInUseError(['name'])))
+  })
+
+  it('Should return 403 if ranklAlreadyUsed is true', async () => {
+    createHeroService.execute.mockResolvedValueOnce({ ...serviceResponse, rankAlreadyUsed: true })
+    const httpResponse = await sut.handle(params)
+    expect(httpResponse).toEqual(forbidden(new PropertyInUseError(['rank'])))
+  })
+
+  it('Should return 403 if nameAlreadyUsed and ranklAlreadyUsed is true', async () => {
+    createHeroService.execute.mockResolvedValueOnce({ ...serviceResponse, nameAlreadyUsed: true, rankAlreadyUsed: true })
+    const httpResponse = await sut.handle(params)
+    expect(httpResponse).toEqual(forbidden(new PropertyInUseError(['name', 'rank'])))
   })
 })
