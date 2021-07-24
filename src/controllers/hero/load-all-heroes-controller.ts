851f502639a1fdb4f1e05530b8cea692bea6a8b7
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-07-24 14:12:21 -0300

    feat: ensure LoadAllHeroesController return 204 when heroes is empty

--- src/controllers/hero/load-all-heroes-controller.ts
+++ src/controllers/hero/load-all-heroes-controller.ts
@@ -1,17 +1,13 @@
 import { IController } from '@/contracts/controllers/controller'
 import { HttpResponse } from '@/contracts/http/http'
 import { ILoadAllHeores } from '@/contracts/services'
-import { ok } from '../http-helper'
+import { noContent, ok } from '../http-helper'
 
 export class LoadAllHeroesController implements IController {
   constructor (private readonly loadAllHeroesService: ILoadAllHeores) {}
 
   async handle (): Promise<HttpResponse> {
     const heroes = await this.loadAllHeroesService.execute()
-    return ok(heroes)
+    return heroes?.length ? ok(heroes) : noContent()
   }
-}
-
-export namespace LoadAllHeroesController {
-
 }
