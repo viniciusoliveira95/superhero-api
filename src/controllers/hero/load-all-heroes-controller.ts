d821e70a4b99ca602a1c3812472a09bb7eba81a0
viniciusoliveira95 <vinicius_goliveira@hotmail.com>
2021-07-24 14:15:41 -0300

    feat: ensure LoadAllHeroesController return 500 when service throws

--- src/controllers/hero/load-all-heroes-controller.ts
+++ src/controllers/hero/load-all-heroes-controller.ts
@@ -1,13 +1,17 @@
 import { IController } from '@/contracts/controllers/controller'
 import { HttpResponse } from '@/contracts/http/http'
 import { ILoadAllHeores } from '@/contracts/services'
-import { noContent, ok } from '../http-helper'
+import { noContent, ok, serverError } from '../http-helper'
 
 export class LoadAllHeroesController implements IController {
   constructor (private readonly loadAllHeroesService: ILoadAllHeores) {}
 
   async handle (): Promise<HttpResponse> {
-    const heroes = await this.loadAllHeroesService.execute()
-    return heroes?.length ? ok(heroes) : noContent()
+    try {
+      const heroes = await this.loadAllHeroesService.execute()
+      return heroes?.length ? ok(heroes) : noContent()
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
